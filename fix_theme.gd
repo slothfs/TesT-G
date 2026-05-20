7e29cc205d989af25221304a52d39ff85608c4aa
AK3 <mjlabishek123@gmail.com>
2026-05-20 19:27:51 +0545

    reeworking on it didnt get the desired result

--- fix_theme.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorScript
-
-func _run():
-	var settings = EditorInterface.get_editor_settings()
-	settings.set_setting("interface/theme/preset", "Default")
-	settings.set_setting("interface/theme/base_color", Color(0.2, 0.2, 0.2))
-	print("Theme reset")
